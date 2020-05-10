ceca82823c3190f8e2074972c851bb0ff4e6e714
Shavkat Metekov <metekov.shavkat@gmail.com>
2020-05-10 16:56:47 -0500

    new connection

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ function App() {
 
   
     <div id="main-content" className="container">
-        <section className="main-section">
+        <section className="main-section" />
     
             <h2>About Me</h2>
             <div className="my-picture">
                 <img src="assets/images/my-pic.JPG" alt="Shavkat Metekov"> </img> 
             </div>
             <div className="text-paragraph">
-                <p>         
+                <p/>         
                     I am from Turkmenistan, Central Asia. 
                     Two years ago graduated Metropolitan State University with BAS in Computer Forensics. I like troubleshooting computer issues so I enjoy my work for the most part
                     I also love nature and looking for bugs. That is why I decided I will work with code and possibly get into developing positions at my work. If you are curious, 
@@ -40,23 +40,18 @@ function App() {
                     and also pain and draw when I get a chance. Once I finish couple paintings I will upload them here. From sports I like to play
                     soccer. Right now, with quarantine, people are not out, but before it started I used to play at least once a week. Hopefully, soon in Summer things will get back to normal.
 
-                    <br /><br>
+                    <br /><br />
                     In my professional life, I am planning to shift into Web Development in near future. I am, probably taking an interesting route
                     to get to where I want to be at because usually people are more decisive when it comes to career. When I was in school, I was interested
                     in security breaches (to some extend I still am), but then I double majored in Network Security that got me into CCNA and Network Admin roles,
                     Then I was put into support because I was eager to solve problems that puzzled developers as well as daily users. Now, my curiosity is taking
                     me into exploring programming and designing web pages as well as web applications. I am eager to learn more!
                  
-                    <p />
     </div>
-    <div>
-    <footer>
-        <div className="container">
-            <p>Copyright© U of M Bootcamp project by Shavkat Metekov</p>
-        </div>
-    </footer>
-    </div>
+   </div>
+   </div>
+    
     );
     }
-  }
+  
 export default App;
